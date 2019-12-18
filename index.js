108457a6a980881ac58180c88f08c115c439bbc5
Jonathan Weibel <58305699+jonathan-weibel-sonarsource@users.noreply.github.com>
2019-12-18 09:33:36 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+(function() {
+  let toto = 1;
+  const tutu = "HJJGFJGJHKH";
+  toto = "Jean-Claude Dusse";
+
+  coucou(toto);
+ 
+  function coucou(blabla) {
+    console.log('Coucou,', blabla);
+  }
+
+  const tata = 1;
+  const tata = 2;
+
+  console.log("DEBUG", tata);
+
+  function 1coucou() {
+    alert("coucou");
+  }
+})();
